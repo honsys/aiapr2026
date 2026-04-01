8b6dffb308f2842a09c3725822ba7347c2b411e1
David B Hon <davidb.hon@gmail.com>
2026-03-31 20:07:01 -0700

    sync

--- gem26/finance/publicom.py
+++ gem26/finance/publicom.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
-import requests
+import os
 
 _url = "https://api.public.com/userapiauthservice/personal/access-tokens"
-#_valid = 60*24*31 # 31 day validity of secret
 _valid = 1440 # max minutes valid allowed as of 31mar2026
-print(_valid, _url)
+_key_secret = os.getenv('PUBLICOM_KEY_ENV')
+if not(_key_secret): _key_secret = 'FxYrtlFIERwC12QZUeQeyuejwBlXwJxf'
+print(_valid, _url, _key_secret)
 
-headers = {
-    "Content-Type": "application/json",
-    "User-Agent": "public-dev-docs"
-}
+def gen_token():
+  import requests
+  headers = {"Content-Type": "application/json", "User-Agent": "public-dev-docs"}
+  request_body = {"validityInMinutes": _valid,"secret": 'FxYrtlFIERwC12QZUeQeyuejwBlXwJxf'}
+  response = requests.post(_url, headers=headers, json=request_body)
+  data = response.json() ; print(data)
+  return data
 
-request_body = {
-  "validityInMinutes": _valid,
-  "secret": "string"
-}
+def main(args):
+  print(args)
+  token = gen_token() ; print(token)
 
-response = requests.post(_url, headers=headers, json=request_body)
-data = response.json()
-print(data)
+if __name__ == "__main__":
+  import sys
+  main(sys.argv[1:])
+
+
 
