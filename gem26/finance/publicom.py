58309c04318177e301b806155d550035c50a7ec5
David B Hon <davidb.hon@gmail.com>
2026-04-08 20:01:00 -0700

    sync

--- gem26/finance/publicom.py
+++ gem26/finance/publicom.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import os
 
 _url = "https://api.public.com/userapiauthservice/personal/access-tokens"
 _valid = 1440 # max minutes valid allowed as of 31mar2026
-_key_secret = os.getenv('PUBLICOM_KEY_ENV')
-if not(_key_secret): _key_secret = 'FxYrtlFIERwC12QZUeQeyuejwBlXwJxf'
+_key_secret = None
+
 print(_valid, _url, _key_secret)
 
-def gen_token():
+def gen_token(key=_key_secret):
   import requests
   headers = {"Content-Type": "application/json", "User-Agent": "public-dev-docs"}
-  request_body = {"validityInMinutes": _valid,"secret": 'FxYrtlFIERwC12QZUeQeyuejwBlXwJxf'}
+  request_body = {"validityInMinutes": _valid,"secret": key}
   response = requests.post(_url, headers=headers, json=request_body)
-  data = response.json() ; print(data)
+  data = response.json() # ; print(data)
   return data
 
 def main(args):
-  print(args)
-  token = gen_token() ; print(token)
+  _key_secret = os.getenv('PUBLICOM_KEY_ENV') # ; print(_key_secret, args)
+  token = gen_token(_key_secret) ; print(token)
 
 if __name__ == "__main__":
   import sys
